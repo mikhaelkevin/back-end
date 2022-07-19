e5a576349ea04c547f8de4786b0a3308de5497bd
Mikhael <goodboybutnot@gmail.com>
2022-07-20 04:46:00 +0700

    [FIX] Data response
    -Change data response from object to array

--- app/controllers/searchControllers.js
+++ app/controllers/searchControllers.js
@@ -8,28 +8,28 @@ const getCandidates = async (req, res) => {
   const value = req?.query?.value?.toLowerCase() ?? '';
 
   const sortByRules = sortBy !== 'name' && sortBy !== 'domicile' && sortBy !== 'skill' && sortBy !== '';
-  if (sortByRules) throw new ErrorResponse('invalid sort type');
+  if (sortByRules) throw new ErrorResponse('Invalid sort type');
 
   const sortByNameRules = ((!sortBy && value) || sortBy === 'name');
   if (sortByNameRules) {
     const getCandidateResults = await getCandidateByName(value);
-    return res.status(200).send({ message: 'Sort by name success!', data: getCandidateResults.rows });
+    return res.status(200).send(getCandidateResults.rows);
   }
 
   const sortByDomicileRules = (sortBy === 'domicile');
   if (sortByDomicileRules) {
     const getCandidateResults = await getCandidateByDomicile(value);
-    return res.status(200).send({ message: 'Sort by domicile success!', data: getCandidateResults.rows });
+    return res.status(200).send(getCandidateResults.rows);
   }
 
   const sortBySkillRules = (sortBy === 'skill');
   if (sortBySkillRules) {
     const getCandidateResults = await getCandidateBySkills(value);
-    return res.status(200).send({ message: 'Data found!', data: getCandidateResults.rows });
+    return res.status(200).send(getCandidateResults.rows);
   }
 
   const getAllCandidateResults = await getAllCandidates();
-  res.status(200).send({ message: 'Get all candidates success!', data: getAllCandidateResults.rows });
+  res.status(200).send(getAllCandidateResults.rows);
 };
 
 module.exports = { getCandidates };
