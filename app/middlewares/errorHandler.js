5311340d459ec26b605b8e8a3457bd6a0f963310
Mikhael <goodboybutnot@gmail.com>
2022-07-20 01:39:29 +0700

    [ADD] Feature search candidates as recruiters
    -Create new endpoint on routes
    -Create new models for search
    -Create new controllers for search

--- app/middlewares/errorHandler.js
+++ app/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const errorHandler = (error, req, res, next) => {
+  console.log('Error =>', error);
   res.status(error.status || 500).send({
     message: error.message || 'Internal server error'
   });
