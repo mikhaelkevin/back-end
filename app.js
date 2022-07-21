4fc913910f83947cba79a43095c8c3b3adec9c3c
Mikhael <goodboybutnot@gmail.com>
2022-07-21 12:10:55 +0700

    [FEATURE] HIRE FEATURE

--- app.js
+++ app.js
@@ -19,21 +19,13 @@ app.use(bodyParser.json());
 // ROUTES DECLARATION
 const usersRoutes = require('./routes/usersRoutes');
 const testimonialsRoutes = require('./routes/testimonialsRoutes');
-<<<<<<< HEAD
-const authRoutes = require('./routes/authRoutes');
-=======
 const hiresRoutes = require('./routes/hiresRoutes');
->>>>>>> 402b94abaf4960f1b9aea0d23ebd1ffcebed5b94
 
 // ROUTES
 app.use('/', corsWithAllowList, usersRoutes);
 app.use('/testimonials', corsWithAllowList, testimonialsRoutes);
-<<<<<<< HEAD
-app.use('/auth', corsWithAllowList, authRoutes);
-=======
 app.use('/hires', corsWithAllowList, hiresRoutes);
 app.use('/auth', corsWithAllowList, usersRoutes);
->>>>>>> 402b94abaf4960f1b9aea0d23ebd1ffcebed5b94
 
 app.use(errorHandler);
 
