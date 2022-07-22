6689b8653af1fb8b3e243e067721ea99981f8873
Mikhael <goodboybutnot@gmail.com>
2022-07-22 14:19:51 +0700

    [FIX] MAJOR BUG
    -Delete unecessary model on User
    -Fix database structure recruiter id and candidate id equals to their user id

--- app/models/User.js
+++ app/models/User.js
@@ -1,27 +1,6 @@
 const db = require('../../configs/database');
 const { ErrorResponse } = require('../../utils/errorResponse');
 
-const registerRecruiterModel = (person) => {
-  return new Promise((resolve, reject) => {
-    db.query('INSERT INTO users (email, password, role_id) VALUES ($1, $2, $3) RETURNING *',
-      [person.email, person.password, person.roleId], (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          const userId = result.rows[0].id;
-          db.query('INSERT INTO recruiter_profiles (company_name, company_field, phonenumber, user_id) VALUES ($1, $2, $3, $4) RETURNING company_name',
-            [person.name, person.companyField, person.phonenumber, userId], (_error, _result) => {
-              if (_error) {
-                reject(_error);
-              } else {
-                resolve(_result);
-              }
-            });
-        }
-      });
-  });
-};
-
 const getUserById = (userId) => {
   return new Promise((resolve, reject) => {
     db.query('SELECT * FROM users WHERE id=$1',
@@ -30,27 +9,6 @@ const getUserById = (userId) => {
         if (error) return reject(error);
         if (!result.rowCount) return reject(new ErrorResponse('User not found!', 404));
         resolve(result);
-      });
-  });
-};
-
-const registerCandidateModel = (person) => {
-  return new Promise((resolve, reject) => {
-    db.query('INSERT INTO users (email, password, role_id) VALUES ($1, $2, $3) RETURNING *',
-      [person.email, person.password, person.roleId], (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          const userId = result.rows[0].id;
-          db.query('INSERT INTO candidate_profiles (name, phonenumber, user_id) VALUES ($1, $2, $3) RETURNING name',
-            [person.name, person.phonenumber, userId], (_error, _result) => {
-              if (_error) {
-                reject(_error);
-              } else {
-                resolve(_result);
-              }
-            });
-        }
       });
   });
 };
@@ -112,8 +70,6 @@ const getRecruiterByUserId = (recruiterId) => {
 };
 
 module.exports = {
-  registerRecruiterModel,
-  registerCandidateModel,
   getUserEmailModel,
   getUserById,
   getRecruiterById,
