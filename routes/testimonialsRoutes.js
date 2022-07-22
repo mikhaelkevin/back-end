994d60af0c257c5a1200225fb3905a026fda2510
Mikhael <goodboybutnot@gmail.com>
2022-07-23 00:43:33 +0700

    private routes using JWToken

--- routes/testimonialsRoutes.js
+++ routes/testimonialsRoutes.js
@@ -2,12 +2,13 @@
 const testimonialsRoutes = require('express').Router();
 const urlencoded = require('body-parser').urlencoded({ extended: false });
 const asyncHandler = require('../app/middlewares/asyncHandler');
+const { authorizationTokenHandler } = require('../app/middlewares/tokenHandler');
 
 // CONTROLLER DECLARATIONS
 const { addTestimonial, getTestimonials } = require('../app/controllers/testimonialsControllers');
 
 // ROUTE ENDPOINTS
-testimonialsRoutes.post('/', urlencoded, asyncHandler(addTestimonial))
+testimonialsRoutes.post('/', authorizationTokenHandler, urlencoded, asyncHandler(addTestimonial))
   .get('/', asyncHandler(getTestimonials));
 
 module.exports = testimonialsRoutes;
