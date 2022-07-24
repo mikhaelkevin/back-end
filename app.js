1ba50ac13ead83a08008842a77ed4695145e9a2a
Mikhael <goodboybutnot@gmail.com>
2022-07-24 14:52:57 +0700

    move cors back and add json script to start

--- app.js
+++ app.js
@@ -11,20 +11,8 @@ const port = process.env.PORT || process.env.LOCAL_PORT;
 
 // MIDDLEWARES
 const { errorHandler } = require('./app/middlewares/errorHandler');
+const corsWithAllowList = require('./configs/cors');
 const { authorizationTokenHandler } = require('./app/middlewares/tokenHandler');
-
-// CORS
-const cors = require('cors');
-const allowlist = ['https://joshy-app.herokuapp.com', 'http://localhost:3001', 'http://localhost:3000'];
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(bodyParser.json());
@@ -36,12 +24,12 @@ const authRoutes = require('./routes/authRoutes');
 const profilesRoutes = require('./routes/profilesRoutes');
 
 // PUBLIC ROUTES
-app.use('/auth', cors(corsOptionsDelegate), authRoutes);
-app.use('/testimonials', cors(corsOptionsDelegate), testimonialsRoutes);
+app.use('/auth', corsWithAllowList, authRoutes);
+app.use('/testimonials', corsWithAllowList, testimonialsRoutes);
 
 // PRIVATE ROUTES JWT REQUIRED
-app.use('/', cors(corsOptionsDelegate), authorizationTokenHandler, usersRoutes);
-app.use('/profile', cors(corsOptionsDelegate), authorizationTokenHandler, profilesRoutes);
+app.use('/', corsWithAllowList, authorizationTokenHandler, usersRoutes);
+app.use('/profile', corsWithAllowList, authorizationTokenHandler, profilesRoutes);
 
 app.use('*', (req, res) => {
   res.status(200).send('Connected');
