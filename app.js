c32b181b75b80f711dc37d4faa39e077ab069330
Mikhael <goodboybutnot@gmail.com>
2022-09-05 21:20:30 +0700

    fix(app): fix cors allowed url to all

--- app.js
+++ app.js
@@ -16,18 +16,19 @@ const { authorizationTokenHandler } = require('./app/middlewares/tokenHandler');
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // CORS CONFIGURATION
-const allowlist = ['https://joshy-app.web.app/', 'http://localhost:3001', 'http://localhost:3000'];
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
+// const allowlist = ['https://joshy-app.web.app/', 'http://localhost:3001', 'http://localhost:3000'];
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
 
 // ROUTES DECLARATION
 const usersRoutes = require('./routes/usersRoutes');
@@ -36,12 +37,12 @@ const authRoutes = require('./routes/authRoutes');
 const profilesRoutes = require('./routes/profilesRoutes');
 
 // PUBLIC ROUTES
-app.use('/auth', cors(corsOptionsDelegate), authRoutes);
-app.use('/testimonials', cors(corsOptionsDelegate), testimonialsRoutes);
+app.use('/auth', authRoutes);
+app.use('/testimonials', testimonialsRoutes);
 
 // PRIVATE ROUTES JWT REQUIRED
-app.use('/', cors(corsOptionsDelegate), authorizationTokenHandler, usersRoutes);
-app.use('/profile', cors(corsOptionsDelegate), authorizationTokenHandler, profilesRoutes);
+app.use('/', authorizationTokenHandler, usersRoutes);
+app.use('/profile', authorizationTokenHandler, profilesRoutes);
 
 app.use('*', (req, res) => {
   res.status(200).send('Connected');
