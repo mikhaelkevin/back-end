f5525efb39ded0a76000b5469e5d2dd4325b4a14
Mikhael <goodboybutnot@gmail.com>
2022-09-05 21:15:20 +0700

    fix(app): move cors settings to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 // CONFIG
@@ -11,11 +12,22 @@ const port = process.env.PORT || process.env.LOCAL_PORT;
 
 // MIDDLEWARES
 const { errorHandler } = require('./app/middlewares/errorHandler');
-const corsWithAllowList = require('./configs/cors');
 const { authorizationTokenHandler } = require('./app/middlewares/tokenHandler');
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(bodyParser.json());
+
+// CORS CONFIGURATION
+const allowlist = ['https://joshy-app.web.app/', 'http://localhost:3001', 'http://localhost:3000'];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 // ROUTES DECLARATION
 const usersRoutes = require('./routes/usersRoutes');
@@ -24,12 +36,12 @@ const authRoutes = require('./routes/authRoutes');
 const profilesRoutes = require('./routes/profilesRoutes');
 
 // PUBLIC ROUTES
-app.use('/auth', corsWithAllowList, authRoutes);
-app.use('/testimonials', corsWithAllowList, testimonialsRoutes);
+app.use('/auth', cors(corsOptionsDelegate), authRoutes);
+app.use('/testimonials', cors(corsOptionsDelegate), testimonialsRoutes);
 
 // PRIVATE ROUTES JWT REQUIRED
-app.use('/', corsWithAllowList, authorizationTokenHandler, usersRoutes);
-app.use('/profile', corsWithAllowList, authorizationTokenHandler, profilesRoutes);
+app.use('/', cors(corsOptionsDelegate), authorizationTokenHandler, usersRoutes);
+app.use('/profile', cors(corsOptionsDelegate), authorizationTokenHandler, profilesRoutes);
 
 app.use('*', (req, res) => {
   res.status(200).send('Connected');
